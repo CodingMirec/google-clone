91e254aed70e3d8d8afb7578b0396476510c4191
CodingMirec <m.sykora01@gmail.com>
2021-04-13 17:35:26 +0200

    keys for APIs

---
+++ keys.js
@@ -0,0 +1,5 @@
+//API KEY https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyCobrtlJkVTgnYdnJpVRefw2csFWHrg5KU";
+
+//CONTEXT KEY https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "30a458a77bb90eae2";
